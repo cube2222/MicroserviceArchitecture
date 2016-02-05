f52b323e4f2108cd32f222442506a47325202f12
Jakub Martin <jakub.wit.martin@gmail.com>
2016-02-05 17:27:20 +0100

    Routes for ImageService

--- ImageService/APIInterface.go
+++ ImageService/APIInterface.go
@@ -1,5 +1,36 @@
 package main
 
-func setUpRoutes() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func setUpRoutes() {
+	http.HandleFunc("/newImage", newImage)
+	http.HandleFunc("/getImage", getImage)
+	http.HandleFunc("/finishedImage", finishedImage)
+}
+
+func newImage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		//Do stuff...
+	} else {
+		fmt.Fprintln(w, "ERROR: Only POST accepted.")
+	}
+}
+
+func getImage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "GET" {
+		//Do stuff...
+	} else {
+		fmt.Fprintln(w, "ERROR: Only GET accepted.")
+	}
+}
+
+func finishedImage(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		//Do stuff...
+	} else {
+		fmt.Fprintln(w, "ERROR: Only POST accepted.")
+	}
 }
